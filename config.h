e9a37ebcdfca435ecab5a15abcb1d775d880efbb
kurisu170 <ivan.popovich000@gmail.com>
2020-01-01 21:21:50 +0200

    Forgot that i just binded music player to p, since i dont use m anyway just let it be

--- config.h
+++ config.h
@@ -76,7 +76,7 @@ static Key keys[] = {
     { MODKEY,                       XK_q,      killclient,     {0} },
     { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
     { MODKEY|ShiftMask,             XK_space,  setlayout,      {.v = &layouts[1]} },
-  /*{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },*/
+    { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
     { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
     { MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
     { MODKEY,                       XK_s,      togglesticky,   {0} },
